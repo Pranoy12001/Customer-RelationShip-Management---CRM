7498816cb0c72292eb22172ef6e02d33e67c6111
Pranoy <pranoy.mbstu12001@gmail.com>
2019-09-28 23:22:57 +0600

    create customer entity class and also create customerDAO interface

---
+++ src/com/pranoy/springdemo/dao/CustomerDAO.java
@@ -0,0 +1,9 @@
+package com.pranoy.springdemo.dao;
+
+import java.util.List;
+
+import com.pranoy.springdemo.entity.Customer;
+
+public interface CustomerDAO {
+	public List<Customer> getCustomers();
+}
